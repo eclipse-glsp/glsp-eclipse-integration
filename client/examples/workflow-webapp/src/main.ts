c4a25963d433af1d36c7a0d1f716284e65c0240d
Tobias Ortmayr <tortmayr@eclipsesource.com>
2021-08-17 10:42:59 +0200

    #315 Refactor base protocol (#86)
    - Update targetplatform to use latest glsp versions and
    Adapt cllient code to conform to the changes of :
    eclipse-glsp/glsp-client#132
    
    Part-of eclipse-glsp/glsp/issues/315

--- client/examples/workflow-webapp/src/main.ts
+++ client/examples/workflow-webapp/src/main.ts
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2020 EclipseSource and others.
+ * Copyright (c) 2020-2021 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -16,14 +16,15 @@
 import 'reflect-metadata';
 
 import {
+    configureServerActions,
     EnableToolPaletteAction,
     GLSPActionDispatcher,
     GLSPDiagramServer,
-    InitializeClientSessionAction,
+    IActionDispatcher,
     RequestTypeHintsAction
 } from '@eclipse-glsp/client';
 import { getParameters } from '@eclipse-glsp/ide';
-import { ApplicationIdProvider, BaseJsonrpcGLSPClient, JsonrpcGLSPClient } from '@eclipse-glsp/protocol';
+import { ApplicationIdProvider, BaseJsonrpcGLSPClient, GLSPClient, JsonrpcGLSPClient } from '@eclipse-glsp/protocol';
 import { CenterAction, RequestModelAction, TYPES } from 'sprotty';
 
 import createContainer from './di.config';
@@ -35,7 +36,7 @@ const filePath = urlParameters.path;
 // and/or Eclipse Servers may be running in parallel (e.g. 1/Eclipse IDE)
 const port = parseInt(urlParameters.port, 10);
 const id = 'workflow';
-const name = 'Workflow Diagram';
+const diagramType = 'workflow-diagram';
 const websocket = new WebSocket(`ws://localhost:${port}/${id}`);
 
 const clientId = urlParameters.client || ApplicationIdProvider.get();
@@ -46,25 +47,32 @@ const container = createContainer(widgetId);
 const diagramServer = container.get<GLSPDiagramServer>(TYPES.ModelSource);
 diagramServer.clientId = clientId;
 
-const actionDispatcher = container.get<GLSPActionDispatcher>(TYPES.IActionDispatcher);
-
 websocket.onopen = () => {
     const connectionProvider = JsonrpcGLSPClient.createWebsocketConnectionProvider(websocket);
-    const glspClient = new BaseJsonrpcGLSPClient({ id, name, connectionProvider });
-    diagramServer.connect(glspClient).then(client => {
-        client.initializeServer({ applicationId: ApplicationIdProvider.get() });
-        actionDispatcher.dispatch(new InitializeClientSessionAction(diagramServer.clientId));
-        actionDispatcher.dispatch(new RequestModelAction({
-            // Java's URLEncoder.encode encodes spaces as plus sign but decodeURI expects spaces to be encoded as %20.
-            // See also https://en.wikipedia.org/wiki/Query_string#URL_encoding for URL encoding in forms vs generic URL encoding.
-            sourceUri: 'file://' + decodeURI(filePath.replace(/\+/g, '%20')),
-            diagramType: 'workflow-diagram'
-        }));
-        actionDispatcher.dispatch(new RequestTypeHintsAction('workflow-diagram'));
-        actionDispatcher.dispatch(new EnableToolPaletteAction());
-        actionDispatcher.onceModelInitialized().then(() => actionDispatcher.dispatch(new CenterAction([])));
-    });
+    const glspClient = new BaseJsonrpcGLSPClient({ id, connectionProvider });
+    initialize(glspClient);
+
 };
+
+async function initialize(client: GLSPClient): Promise<void> {
+    await diagramServer.connect(client);
+    const result = await client.initializeServer({ applicationId: ApplicationIdProvider.get(), protocolVersion: GLSPClient.protocolVersion });
+    await configureServerActions(result, diagramType, container);
+
+    const actionDispatcher = container.get<IActionDispatcher>(TYPES.IActionDispatcher) as GLSPActionDispatcher;
+
+    await client.initializeClientSession({ clientSessionId: diagramServer.clientId, diagramType });
+
+    actionDispatcher.dispatch(new RequestModelAction({
+        // Java's URLEncoder.encode encodes spaces as plus sign but decodeURI expects spaces to be encoded as %20.
+        // See also https://en.wikipedia.org/wiki/Query_string#URL_encoding for URL encoding in forms vs generic URL encoding.
+        sourceUri: 'file://' + decodeURI(filePath.replace(/\+/g, '%20')),
+        diagramType: 'workflow-diagram'
+    }));
+    actionDispatcher.dispatch(new RequestTypeHintsAction('workflow-diagram'));
+    actionDispatcher.dispatch(new EnableToolPaletteAction());
+    actionDispatcher.onceModelInitialized().then(() => actionDispatcher.dispatch(new CenterAction([])));
+}
 
 function setWidgetId(mainWidgetId: string): void {
     const mainWidget = document.getElementById('sprotty');
